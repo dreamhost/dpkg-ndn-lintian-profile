654aab8acf26c57bc10031d8fc99505080fe640b
Chris Weyl <chris.weyl@dreamhost.com>
2015-03-25 21:03:06 -0700

    remove superfluous debian/ templates

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(ndn-lintian-profile):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="ndn-lintian-profile" command="/usr/bin/ndn-lintian-profile"
--- debian/ndn-lintian-profile.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the ndn-lintian-profile package
-#
-0 4	* * *	root	[ -x /usr/bin/ndn-lintian-profile_maintenance ] && /usr/bin/ndn-lintian-profile_maintenance
